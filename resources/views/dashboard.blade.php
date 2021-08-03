eb8ef2ed0a7c789ac7bdd9ebbefdac0193611aa4
unknown <joseluis95123@gmail.com>
2021-08-03 23:44:44 +0200

    title changed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
             <h3 class="text-gray-800 text-2xl text-center font-bold">
-                top rated movies
+                top 100 rated movies
             </h3>
             <x-movieweb.general.search></x-movieweb.general.search>
             <div class="grid grid-cols-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
