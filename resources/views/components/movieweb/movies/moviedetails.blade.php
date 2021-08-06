0afbc54bfd7c460f56ef6ab4bbab894a8ca43287
unknown <joseluis95123@gmail.com>
2021-08-06 19:09:36 +0200

    fav/unfav options added to movies detailed/see more

--- resources/views/components/movieweb/movies/moviedetails.blade.php
+++ resources/views/components/movieweb/movies/moviedetails.blade.php
@@ -12,25 +12,33 @@
         <section class="text-gray-600 body-font">
             <div class="container mx-auto flex px-5 py-22 md:flex-row flex-col items-center">
 
-                <x-movieweb.movies.details>
-                    <x-slot name="poster_path">
-                        {{ $movie_details->poster_path }}
-                    </x-slot>
-                    <x-slot name="category">
-                        {{ $movie_details->genres[0]->name }}
-                    </x-slot>
-                    <x-slot name="title">
-                        {{ $movie_details->title }}
-                    </x-slot>
-                    <x-slot name="overview">
-                        {{ $movie_details->overview }}
-                    </x-slot>
-                    <x-slot name="id">
-                        {{ $movie_details->id }}
-                    </x-slot>
-
-                </x-movieweb.movies.details>
-
+                <section class="text-gray-600 body-font">
+                    <div class="container mx-auto flex px-5 py-18 md:flex-row flex-col items-center">
+                        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                            <img class="object-cover object-center rounded mb-5" alt="" src="http://image.tmdb.org/t/p/w400/{{ $movie_details->poster_path }}">
+                        </div>
+                        <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pl-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">{{ $movie_details->title }}
+                                <p class="text-gray-400 text-lg font-bold">{{ $movie_details->genres[0]->name }}</p>
+                            </h1>
+                            <p class="mb-8 leading-relaxed">{{ $movie_details->overview }}</p>
+                            <div class="flex justify-center">
+                                @if(in_array($movie_details->id, $userFavMovies))
+                                <form action="{{ route('delete', $movie_details->id) }}" method="post">
+                                    @csrf
+                                    <button class="flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Delete from favorites</button>
+                                </form>
+                                @else
+                                <form action="{{ route('store', 
+                                [ $movie_details->id, $movie_details->title,  substr($movie_details->poster_path, 1) ]
+                                ) }}" method="post">
+                                    @csrf
+                                    <button class="flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Add to favorites</button>
+                                </form>
+                                @endif
+                            </div>
+                        </div>
+                </section>
             </div>
         </section>
 
