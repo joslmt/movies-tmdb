daa5d0e81b2d03f9fedcb98e9131f278f23fd7c6
unknown <joseluis95123@gmail.com>
2021-08-07 14:03:38 +0200

    fix back button bug

--- resources/views/components/movieweb/general/profile.blade.php
+++ resources/views/components/movieweb/general/profile.blade.php
@@ -45,9 +45,9 @@
                                             Edit
                                         </button>
                                 </form>
-                                <a href="{{ route('home') }}">
+                                <form action="{{ route('home') }}" method="get">
                                     <button class="ml-2 flex text-white bg-gray-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded">Back</button>
-                                </a>
+                                </form>
                             </div>
                         </div>
                     </div>
